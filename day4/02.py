7594971d6dd0f07987785236b2f7d4a87ac8cf1e
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-12-07 09:37:20 +0100

    Part Two

--- day4/02.py
+++ day4/02.py
@@ -5,21 +5,29 @@ f = open("day4/input.txt","r")
 lines = f.read().splitlines()
 wNumbers = []
 cardNums = []
-total = 0
 
+def GetMatches(winNums, cNums):
+    matches = 0 
+    for x in cNums:
+        if x in winNums:
+            matches += 1
+    return matches
+    
 for x in lines:
     y = re.sub(r'Card[\s]+[\d]+: ',"",x). split(" | ") # Replacing Card `number` with ""
     wNumbers.append(list(map(int,re.findall(r'[\d]+', y[0])))) #append list of winning numbers
     cardNums.append(list(map(int,re.findall(r'[\d]+', y[1])))) #append list of your numbers
 
-for i, x in enumerate(cardNums):
-    winners = 0
-    points = 0
-    for y in x:
-        if y in wNumbers[i]:
-            winners+=1
-    if winners > 0:
-        points = pow(2,winners-1)
-    total += points
+matchNum = []
+copyNum = [1] * len(wNumbers)
 
-print(total)
+for i, x in enumerate(cardNums):
+    matchNum.append(GetMatches(wNumbers[i],x))
+
+for i, x in enumerate(matchNum):
+    for k in range (0, copyNum[i]):
+        for j in range(i,x+i):
+            copyNum[j+1] += 1
+
+
+print(sum(copyNum))
